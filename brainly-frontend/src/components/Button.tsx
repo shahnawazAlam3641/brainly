9de172520903433a8764cd6cdb03f8610dfee879
shahnawazAlam3641 <shahnawazalam3641@gmail.com>
2024-12-12 19:01:54 +0530

    add hero page

--- brainly-frontend/src/components/Button.tsx
+++ brainly-frontend/src/components/Button.tsx
@@ -1,21 +1,28 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement } from "react";
 
 interface buttonProp {
-    startIcon?:ReactElement,
-    text:string,
-    endIcon?:ReactElement,
-    isPrimary:boolean,
-    onPress?:()=> void
+  startIcon?: ReactElement;
+  text: string;
+  endIcon?: ReactElement;
+  isPrimary: boolean;
+  onPress?: () => void;
 }
 
-const Button = (props:buttonProp) => {
+const Button = (props: buttonProp) => {
   return (
-    <button onClick={props.onPress} className={`flex gap-2 items-center justify-center  px-3 py-2 w-fit h-fit rounded-md hover:scale-95 transition-all duration-200 ${props.isPrimary ? "text-white bg-purple-600" : " text-purple-600 bg-purple-200"}`}> 
-        {props.startIcon && <span>{props.startIcon}</span>}
-        {props.text && <span>{props.text}</span>}
-        {props.endIcon && <span>{props.endIcon}</span>}
+    <button
+      onClick={props.onPress}
+      className={`flex gap-2 items-center justify-center px-2 py-2 text-sm md:text-md  md:px-3 md:py-2 w-fit h-fit rounded-md hover:scale-95 transition-all duration-200 ${
+        props.isPrimary
+          ? "text-white bg-purple-600"
+          : " text-purple-600 bg-purple-200"
+      }`}
+    >
+      {props.startIcon && <span>{props.startIcon}</span>}
+      {props.text && <span>{props.text}</span>}
+      {props.endIcon && <span>{props.endIcon}</span>}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- brainly-frontend/src/components/Hero.tsx
+++ brainly-frontend/src/components/Hero.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import Button from "./Button";
+import { useNavigate } from "react-router";
+import HeroSS from "../assets/HeroSS.png";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Hero
+  return (
+    <div className="min-h-[90vh] bg-slate-50 flex flex-col items-center gap-3 md:gap-6">
+      <h1 className="text-2xl  md:text-5xl font-extrabold text-slate-800 text-center mt-16 max-w-[90vw] md:max-w-[65vw] lg:max-w-[55vw]">
+        Your Digital Vault for Important Links and Notes
+      </h1>
+      <p className="text-sm  md:text-lg  text-slate-500 text-center  max-w-[75vw] md:max-w-[55vw] lg:max-w-[45vw]">
+        Store and organize important content like videos, tweets, documents, and
+        notes. Access them anytime, and easily share with friends via link
+      </p>
+      <div className="flex gap-4">
+        <Button
+          isPrimary={true}
+          onPress={() => navigate("/signin")}
+          text="Get Started"
+        />
+      </div>
+      <img
+        src={HeroSS}
+        className="w-[80vw] max-w-[95vw] rounded-lg border shadow-slate-200 shadow-xl mb-10"
+      />
+    </div>
+  );
+};
+
+export default Hero;
